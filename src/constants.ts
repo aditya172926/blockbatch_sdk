8c3db65fddae27051a6c65a1911012f33a206b49
aditya172926 <aditya26sg@gmail.com>
2024-08-05 19:55:37 +0530

    chore: renaming constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
-export const BATCH_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // deployed on hardhat local node
-export const BATCH_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; //local batch contract
+export const BATCH_PROCESS_CONTRACT_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // deployed on hardhat local node
+export const BATCH_CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; //local batch contract
 export const CHAIN_ID = 31337; // hardhat local node chain id
 export const BICONOMY_BATCH_CONTRACT = "0x0000002512019Dafb59528B82CB92D3c5D2423aC";
 export const PRIVATE_KEY = ""; // hardhat account 1
