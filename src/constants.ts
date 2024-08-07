44bc71885045e8b894a7bec86c7d0e759a8a5ac7
aditya172926 <aditya26sg@gmail.com>
2024-08-07 10:52:20 +0530

    update constants comments

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,6 @@
-export const BATCH_PROCESS_CONTRACT_ADDRESS = "0x7FD154df41ec41336A86Ee53a3F7Fe886E80Efc7"; // deployed on hardhat local node
-export const BATCH_CONTRACT_ADDRESS = "0xb8cBB6a9965A851Dcb88Bb1734231c531a0bcdF1"; //local batch contract
-export const PRIVATE_KEY = ""; // hardhat account 1
-export const TOKEN_CONTRACT_ADDRESS = "0x857616Fbc511212A2a848dA64B4fC3b9678af6F9"; // LOCAL
+export const BATCH_PROCESS_CONTRACT_ADDRESS = "0x7FD154df41ec41336A86Ee53a3F7Fe886E80Efc7";
+export const BATCH_CONTRACT_ADDRESS = "0xb8cBB6a9965A851Dcb88Bb1734231c531a0bcdF1";
 export const DEFAULT_GAS_LIMIT = 300000;
+
+export const PRIVATE_KEY = ""; // hardhat account 1; for tests
+export const TOKEN_CONTRACT_ADDRESS = "0x857616Fbc511212A2a848dA64B4fC3b9678af6F9"; // for tests
